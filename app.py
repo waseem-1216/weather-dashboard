4181611d81af344235988a8f32d54048431a4973
unknown <Waseem@STS-3137.sts.co.in>
2026-01-29 11:59:26 +0530

    Initial commit: basic weather app structure

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # This is a very simple UI using HTML
+    return """
+    <h1>My Weather Dashboard ☁️</h1>
+    <p>Current Status: <b>Online</b></p>
+    <p>DevOps Step: Application Development ✅</p>
+    """
+
+if __name__ == '__main__':
+    app.run(debug=True)
